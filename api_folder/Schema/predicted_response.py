3647c2b9101a17f89184989e0a9c2493b7044c1f
Deepanshu Sharma <deepanshusharma0846@gmail.com>
2026-04-08 02:00:52 +0530

    clened version 2

--- api_folder/Schema/predicted_response.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-class PredictionResponse(BaseModel):
-    predicted_cgpa: float = Field(...,description="The predicted cgpa",json_schema_extra={"example":2.15})
-    current_avg_cgpa: float = Field(...,description="The average cgpa student have",json_schema_extra={"example":3.1})
-    status: str =Field(...,description="Student evaluation",json_schema_extra={"example":" STUDY TIME AND ATTENDANCE YOU ARE AT HIGH RISK"})
-    model_loaded : str = Field(...,description='model version',json_schema_extra={"example":'1.0.0'})
-
